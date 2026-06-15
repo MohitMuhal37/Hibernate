ea8a23feecaa21659a0b39b89ed037b254961a10
Mohit Muhal <mohitmuhal15122003@gmail.com>
2026-06-15 22:13:00 +0530

    initial commit

--- src/main/java/com/Mohit/Main.java
+++ src/main/java/com/Mohit/Main.java
@@ -9,16 +9,19 @@ import org.hibernate.cfg.Configuration;
 
 public class Main {
     public static void main() throws Exception{
-        Student s1 = new Student();
-        Student s2 = null;
+
+        newClass nc = new newClass();
+        nc.setAid(101);
+        nc.setAname("Max");
+        nc.setTech("CSE");
+
+
         Configuration cfg = new Configuration();
-        cfg.addAnnotatedClass(com.Mohit.Student.class);
+        cfg.addAnnotatedClass(com.Mohit.newClass.class);
         cfg.configure();
         SessionFactory sf = cfg.buildSessionFactory();
         Session ss = sf.openSession();
-        s2 = ss.find(Student.class,24);
         ss.close();
         sf.close();
-        System.out.println(s2);
     }
 }
