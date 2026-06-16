bb5487b614eaa3755a95dd23467a6215c22d1101
Mohit Muhal <mohitmuhal15122003@gmail.com>
2026-06-16 21:45:06 +0530

    initial commit

--- src/main/java/com/Mohit/laptop.java
+++ src/main/java/com/Mohit/laptop.java
@@ -3,5 +3,38 @@ package com.Mohit;
 public class laptop {
         private String brand;
         private String model;
-        private int ram;
+        private laptop laptop;
+
+    public String getBrand() {
+        return brand;
+    }
+
+    public void setBrand(String brand) {
+        this.brand = brand;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+
+    public laptop laptop() {
+        return laptop;
+    }
+
+    public void setlaptop(laptop laptop) {
+        this.laptop = laptop;
+    }
+
+    @Override
+    public String toString() {
+        return "laptop{" +
+                "brand='" + brand + '\'' +
+                ", model='" + model + '\'' +
+                ", ram=" + laptop +
+                '}';
+    }
 }
